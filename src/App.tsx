350bfc2babb175687658264908a5280bf86771c3
AndrMshk <andrmshk1@gmail.com>
2022-06-19 22:56:13 +0300

    'fix_import_err'

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { RootType } from './state/store';
 import { TestTodoListsComponent } from './todoList-api-test/TodoLists-api';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Container, Grid, Paper } from '@mui/material';
+import { AddItemForm } from './AddItemForm';
+import { Todolist } from './Todolist';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 export type TodolistsType = {
@@ -45,55 +55,55 @@ function App() {
 
   return (
     <>
-      {/*<Box sx={{ flexGrow: 1 }}>*/}
-      {/*  <AppBar position="static">*/}
-      {/*    <Toolbar>*/}
-      {/*      <IconButton*/}
-      {/*        size="large"*/}
-      {/*        edge="start"*/}
-      {/*        color="inherit"*/}
-      {/*        aria-label="menu"*/}
-      {/*        sx={{ mr: 2 }}*/}
-      {/*      >*/}
-      {/*        <MenuIcon />*/}
-      {/*      </IconButton>*/}
-      {/*      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>*/}
-      {/*        News*/}
-      {/*      </Typography>*/}
-      {/*      <Button color="inherit">Login</Button>*/}
-      {/*    </Toolbar>*/}
-      {/*  </AppBar>*/}
-      {/*</Box>*/}
-      {/*<Container fixed>*/}
-      {/*  <Grid container style={{ padding: '20px' }}>*/}
-      {/*    <AddItemForm*/}
-      {/*      onClick={addTodoListHandler}*/}
-      {/*    />*/}
-      {/*  </Grid>*/}
-      {/*  <Grid container spacing={3}>*/}
-      {/*    {todolists.map(el => {*/}
-      {/*      return (*/}
-      {/*        <Grid*/}
-      {/*          item*/}
-      {/*          key={el.id}*/}
-      {/*        >*/}
-      {/*          <Paper*/}
-      {/*            style={{ padding: '10px' }}*/}
-      {/*          >*/}
-      {/*            <Todolist*/}
-      {/*              todolistId={el.id}*/}
-      {/*              title={el.title}*/}
-      {/*              filter={el.filter}*/}
-      {/*              deleteTodoList={() => deleteTodoListHandler(el.id)}*/}
-      {/*              changeFilterHandler={(filter: FilterValuesType) => changeFilterHandler(el.id, filter)}*/}
-      {/*              changeTodoListTitle={(title) => changeTodoListTitleHandler(el.id, title)}*/}
-      {/*            />*/}
-      {/*          </Paper>*/}
-      {/*        </Grid>*/}
-      {/*      );*/}
-      {/*    })}*/}
-      {/*  </Grid>*/}
-      {/*</Container>*/}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container fixed>
+        <Grid container style={{ padding: '20px' }}>
+          <AddItemForm
+            onClick={addTodoListHandler}
+          />
+        </Grid>
+        <Grid container spacing={3}>
+          {todolists.map(el => {
+            return (
+              <Grid
+                item
+                key={el.id}
+              >
+                <Paper
+                  style={{ padding: '10px' }}
+                >
+                  <Todolist
+                    todolistId={el.id}
+                    title={el.title}
+                    filter={el.filter}
+                    deleteTodoList={() => deleteTodoListHandler(el.id)}
+                    changeFilterHandler={(filter: FilterValuesType) => changeFilterHandler(el.id, filter)}
+                    changeTodoListTitle={(title) => changeTodoListTitleHandler(el.id, title)}
+                  />
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
       <div>
         <h2>TodoListsApi</h2>
         <TestTodoListsComponent />
